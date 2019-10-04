690cf506a14bc40b0f2f5fda5d546dfd84b604ed
GabrielM <gabriel.martinez@inalambra.com>
2019-10-04 14:31:01 -0500

    reverse changes

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -33,23 +33,21 @@ export const checkAuthTimeOut = expirationTime => {
 };
 
 export const auth = (email, password) => {
-    debugger
     return dispatch => {
         dispatch(authStart());
-        dispatch(authSuccess("xa", "ds"))
-        localStorage.setItem("token", "xa");
-        // const user = {
-        //     email: email,
-        //     password: password
-        // };
-        // const url = "inalambria/url";
-        // axios.post(url, user).then(response => {
-        //     localStorage.setItem("userId", response.userId);
-        //     localStorage.setItem("token", response.token);
-        //     localStorage.setItem("expirationTime", response.expirationTime);
-        //     dispatch(checkAuthTimeOut(response.data.expiresIn));
-        //     dispatch(authSuccess(response.userId, response.userId))
-        // }).catch(error => dispatch(authFail(error)));
+     
+        const user = {
+            email: email,
+            password: password
+        };
+        const url = "inalambria/url";
+        axios.post(url, user).then(response => {
+            localStorage.setItem("userId", response.userId);
+            localStorage.setItem("token", response.token);
+            localStorage.setItem("expirationTime", response.expirationTime);
+            dispatch(checkAuthTimeOut(response.data.expiresIn));
+            dispatch(authSuccess(response.userId, response.userId))
+        }).catch(error => dispatch(authFail(error)));
     }
 }
 
@@ -66,18 +64,18 @@ export const authCheckState = () => {
             dispatch(logout());
         } else {
             dispatch(authSuccess("xa", "ds"))
-            // const expirationDate = new Date(localStorage.getItem("expirationDate"));
-            // if (expirationDate <= new Date()) {
-            //     dispatch(logout());
-            // } else {
-            //     const userId = localStorage.getItem("userId");
-            //     dispatch(authSuccess(token, userId));
-            //     dispatch(
-            //         checkAuthTimeOut(
-            //             (expirationDate.getTime() - new Date().getTime()) / 1000
-            //         )
-            //     );
-            // }
+            const expirationDate = new Date(localStorage.getItem("expirationDate"));
+            if (expirationDate <= new Date()) {
+                dispatch(logout());
+            } else {
+                const userId = localStorage.getItem("userId");
+                dispatch(authSuccess(token, userId));
+                dispatch(
+                    checkAuthTimeOut(
+                        (expirationDate.getTime() - new Date().getTime()) / 1000
+                    )
+                );
+            }
         }
     };
 };
